2391e331af9f915dac8cb794692f0548a990db84
Jacob Brunson <github@jacobbrunson.com>
2021-10-23 22:12:58 +0000

    Avoid circular imports

--- jsonschemacodegen/schemawrappers.py
+++ jsonschemacodegen/schemawrappers.py
@@ -3,12 +3,13 @@ This module wraps a schema object in classes which
 augment each schema element with additional methods.
 """
 
-
 import collections
 import copy
 
-from .example_util import bitsNeededForNumber, ExampleIndex
-from .schemafactory import SchemaFactory
+from typing import Optional
+
+from . example_util import bitsNeededForNumber, ExampleIndex
+
 
 class SchemaBase(collections.UserDict):
     """
@@ -544,3 +545,45 @@ class AnyOfFirstMatchSchema(OneOfSchema):
     def __init__(self, initialdata, root=None):
         CombinatorSchemaBase.__init__(self, 'anyOf', initialdata, root)
         assert isinstance(self.data['anyOf'], list)
+
+class SchemaFactory:
+    """
+    Class with one factory method.
+    """
+
+    @staticmethod
+    def CreateSchema(schema:dict, root:Optional[dict]=None) -> SchemaBase:
+        """Returns the schema wrapped in one of the schema wrapper classes that provides
+        additional functionality.
+        """
+
+        if 'type' in schema:
+            if schema['type'] == 'string':
+                if 'enum' in schema:
+                    return StringEnumSchema(schema, root)
+                return StringSchema(schema, root)
+            elif schema['type'] == 'number' or schema['type'] == 'integer':
+                return NumberSchema(schema, root)
+            elif schema['type'] == 'boolean':
+                return BooleanSchema(schema, root)
+            elif schema['type'] == 'null':
+                return NullSchema(schema, root)
+            elif schema['type'] == 'object':
+                return ObjectSchema(schema, root)
+            elif schema['type'] == 'array':
+                return ArraySchema(schema, root)
+            else:
+                raise NotImplementedError(f"The type '{schema['type']}' is not implemented")
+        elif 'allOf' in schema:
+            return AllOfSchema(schema, root)
+        elif 'anyOf' in schema:
+            if 'x-anyOf-codegen-behavior' in schema \
+                    and schema['x-anyOf-codegen-behavior'] == "matchFirst":
+                return AnyOfFirstMatchSchema(schema, root)
+            return AnyOfSchema(schema, root)
+        elif 'oneOf' in schema:
+            return OneOfSchema(schema, root)
+        elif '$ref' in schema:
+            return Reference(schema, root)
+        else:
+            raise NotImplementedError(str(schema))
