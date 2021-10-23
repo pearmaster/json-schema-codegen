2021e2ef55cbc4b134c208e15456e44bb12167e0
Jacob Brunson <github@jacobbrunson.com>
2021-10-23 23:14:17 +0000

    Use jacobsjsondoc for parsing

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -7,14 +7,16 @@ import unittest
 
 from . import context
 from jsonschemacodegen.schemawrappers import NumberSchema, SchemaFactory
+import jacobsjsondoc
 
 class TestWrappers(unittest.TestCase):
 
     def test_integer_wrapper(self):
-        schema = {
+        schema = """{
             "type": "integer"
-        }
-        wrapped = SchemaFactory.CreateSchema(schema)
+        }"""
+        schema_obj = jacobsjsondoc.parse(schema)
+        wrapped = SchemaFactory.CreateSchema(schema_obj)
         self.assertIsInstance(wrapped, NumberSchema)
         example = wrapped.AnExample(None, 1)
         self.assertIsInstance(example, int)
